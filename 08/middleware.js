abe8352b0f4b3749f4693bd65e859ac83a4dfad1
hbj <301085344@qq.com>
2018-01-27 18:48:27 +0800

    提交代码

--- 08/middleware.js
+++ 08/middleware.js
@@ -2,8 +2,10 @@ var url = require('url');
 var routes = {'all':[]};
 var app = {};
 
+// 中间件
 var querystring = function(req, res, next){
 	req.query = url.parse(req.url, true).query;
+	console.log("执行中间件querystring");
 	next();
 }
 
@@ -17,8 +19,8 @@ var cookie = function(req, res, next){
 			cookies[pair[0].trim()] = pair[1];
 		}
 	}
-
 	req.cookies = cookies;
+	console.log("执行中间件cookie");
 	next();
 }
 
@@ -96,33 +98,12 @@ function init(req, res){
 
 	var stacks = match(pathname, routes.all);
 
-	if(routes.hasOwnProperty(method)){
-		stacks.concat(match(pathname, routes[method]));
-	}
-
-	if(stacks.length){
-		handle(req, res, stacks);
-	}else{
-		handle404(req, res);
-	}
+	handle(req, res, stacks);
 }
 
-var getUser = function(){
-	console.log("I am a action");
-}
-var arr = ['get', 'put', 'delete', 'post'];
-arr.forEach( function(method){
-	routes[method] = [];
-	app[method] = function(path, action){
-		routes[method].push([pathRegexp(path), action])
-	}
-});
-
-app.use('/user', querystring);
-app.use(cookie);
-app.get('/user/:username', getUser);
-
-
+// 匹配路由的中间件，提高使用中间件性能
+app.use('/user/:username', querystring);
+app.use('/user/:username',cookie);
 
 var http = require('http');
 http.createServer(init).listen(1337, '127.0.0.1');
