8892db9d3f705caea81f823a250d8a7511700122
hbj <301085344@qq.com>
2018-01-27 15:44:19 +0800

    提交

--- 08/mime.js
+++ 08/mime.js
@@ -5,5 +5,5 @@ http.createServer(function (req, res) {
   // 或者
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('<html><body>Hello World</body></html>\n');
-}).listen(1337);
+}).listen(1337);  
 console.log('Server running at http://127.0.0.1:1337/');
